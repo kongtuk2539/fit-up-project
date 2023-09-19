e9a9e4d1238a73acd8f4efaad9b9eb794c163544
kongtuk2539 <136842573+kongtuk2539@users.noreply.github.com>
2023-09-19 14:00:45 +0700

    Dev/tho add circle (#5)
    * add circle-#1
    
    * add circle-#2
    
    * add circle-#3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Circle from './components/circle/Circle.jsx'
 import React from "react";
 import Rightsection from "./components/activitycard/Rightsection";
 
 function App() {
   return (
     <div>
+      <h1 className="font-orbitron font-bold text-6xl">Fit-up</h1>
+      <Circle />
       {/* <h1 className="font-orbitron font-bold text-6xl">Fit-up</h1> */}
 
       {/* this is my content */}
