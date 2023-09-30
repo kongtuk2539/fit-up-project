d880edbfbbb8baef51d6ddaee8645c0865fec659
kongtuk2539 <thoty2539@gmail.com>
2023-10-01 04:32:50 +0700

    circle-and-bar-responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Circle from './components/circle/Circle.jsx'
+import HorizonBar from './components/circle/HorizonBar.jsx'
 import React from "react";
 import Rightsection from "./components/activitycard/Rightsection";
 
 function App() {
   return (
     <div>
-      <h1 className="font-orbitron font-bold text-6xl">Fit-up</h1>
+      {/* <h1 className="font-orbitron font-bold text-6xl">Fit-up</h1> */}
       <Circle />
       {/* <h1 className="font-orbitron font-bold text-6xl">Fit-up</h1> */}
 
