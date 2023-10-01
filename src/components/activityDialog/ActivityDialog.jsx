4206ff817926bba0913f1d0eb34d6b460cd79024
kongtuk2539 <thoty2539@gmail.com>
2023-10-02 03:49:41 +0700

    add calendar

--- src/components/activityDialog/ActivityDialog.jsx
+++ src/components/activityDialog/ActivityDialog.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Datepicker from "react-tailwindcss-datepicker";
 
 const ActivityDialog = () => {
     const [desc, setDesc] = useState('');
@@ -8,6 +9,18 @@ const ActivityDialog = () => {
         const newDesc = e.target.value;
         setDesc(newDesc);
     };
+
+    // Date
+    const [value, setValue] = useState({
+        startDate: new Date(),
+        endDate: new Date().setMonth(11)
+    });
+
+    const handleValueChange = (newValue) => {
+        console.log("newValue:", newValue);
+        setValue(newValue);
+    }
+
 
     return (
         <div className='h-768 w-375 bg-black-medium'>
@@ -100,15 +113,28 @@ const ActivityDialog = () => {
             </div>
 
             {/* Date and Duration */}
-            <div className='h-[75px] w-343 mx-4 mt-6'>
-
-                <div class="relative max-w-sm">
-                    <div class="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
-                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-                        </svg>
-                    </div>
-                    <input datepicker type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date" />
+            <div className='h-[75px] w-343 mx-4 mt-8'>
+                <div>
+                    <p className='text-white font-roboto-mono text-xs mb-2'>
+                        Date
+                    </p>
+                    <Datepicker
+                        primaryColor={"fuchsia"}
+                        placeholder={"Date"}
+                        popoverDirection="up"
+                        containerClassName="relative h-12 w-343 color"
+                        inputClassName="h-12 w-[160px] rounded focus:ring-0 text-white font-roboto-mono text-xs 
+                    bg-black-dark p-3 dark:bg-green-900 dark:placeholder:text-green-100"
+                        toggleClassName="absolute rounded-r-lg text-white left-[115px] h-full px-3 focus:outline-none disabled:opacity-40 disabled:cursor-not-allowed"
+                        value={value}
+                        onChange={handleValueChange}
+                        // showShortcuts={true}
+                        calendarClassName={"bg-black-dark"}
+                        useRange={false}
+                        asSingle={true}
+                        minDate={new Date()}
+                        displayFormat={"DD MMM,YYYY"}
+                    />
                 </div>
 
             </div>
