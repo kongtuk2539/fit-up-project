c2f62fcf936e0d580fbf6bc9e8c8b3785ba89e39
Mallika Nilnoisri <137501517+mallikanns@users.noreply.github.com>
2023-10-02 23:28:57 +0700

    dev/mook-021023 (#7)

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,15 @@ import Sideham from './components/sidebar-ham/Sideham.jsx';
 import Sidebar from './components/sidebar-ham/Sidebar.jsx';
 function App() {
   return (
-    <>
-      <Sideham />
-    </>
+    <div>
+      {/* <h1 className="font-orbitron font-bold text-6xl">Fit-up</h1> */}
+      {/* <Circle /> */}
+      {/* <h1 className="font-orbitron font-bold text-6xl">Fit-up</h1> */}
+
+      {/* this is my content */}
+      <Rightsection/>
+
+    </div>
   )
 }
 
--- src/components/activitycard/Default.jsx
+++ src/components/activitycard/Default.jsx
@@ -2,10 +2,11 @@ import React from "react";
 
 const Default = () => {
   return (
-    // <div className="bg-pink-op10 w-343 h-444 my-6 flex flex-col justify-center items-center border border-dashed border-pink rounded-lg">
-    <div className="borderdefault w-343 h-444 my-6 flex flex-col justify-center items-center">
-      <img src="./src/assets/image/Activity/Dollar.png" alt="Dollar" />
-      <div>
+    <div className="relative borderdefault w-343 h-444 mt-6 flex flex-col items-center px-10 pt-55 lg:h-856 lg:justify-center">
+      <img src="./src/assets/image/Activity/bgBluecoin.png" className="absolute h-full w-full bottom-6 lg:top-36 lg:h-auto" alt="Dollar" />
+      {/* <img src="./src/assets/image/Activity/bluecoin.png" className="z-50 w-[142px] h-[158px] border border-white" alt="Dollar" /> */}
+      <img src="./src/assets/image/Activity/Dollar.png" className="z-50" alt="Dollar" />
+      <div className="z-50">
         <h3 className="font-orbitron text-xl font-bold w-228 h-30 text-center mt-11 mb-2">
           Create an Activity
         </h3>
@@ -13,7 +14,7 @@ const Default = () => {
           Let's Turn Activity into Earnings
         </p>
       </div>
-      <div>
+      <div className="z-50 clickbutton">
         <button className="flex justify-center items-center bg-pink w-263 h-12 text-black-dark font-roboto-mono font-bold">
           <span class="material-symbols-outlined pr-2.5 ">add_circle</span>
           Create new activity
