a9a830650faad20300ede09c7fd2731188336bf3
Jinbe2543 <wuttipat.opor@gmail.com>
2023-10-03 22:13:48 +0700

    dev/Helpopor

--- src/components/bar-chart/CustomBar.jsx
+++ src/components/bar-chart/CustomBar.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const CustomBar = ({ x, y, width, height, fill }) => {
     const [hovered, setHovered] = useState(false);
     const borderRadius = 8; // Adjust the radius for the rounded top corners
-
+    
     return (
         <g>
             <path d={`M${x},${y + height} L${x},${y + borderRadius} Q${x},${y} ${x + borderRadius},${y} L${x + width - borderRadius},${y} 
