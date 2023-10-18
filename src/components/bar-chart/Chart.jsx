86b514074fb106f54b95e525c9b52471b6c9bde6
Sanisa Imsiri <minuent@gmail.com>
2023-10-18 09:45:57 +0700

    New mint (#26)
    * update 17-10-23
    
    * update 18-10-23

--- src/components/bar-chart/Chart.jsx
+++ src/components/bar-chart/Chart.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   BarChart,
   Bar,
@@ -7,48 +7,53 @@ import {
   CartesianGrid,
   Tooltip,
 } from "recharts";
-import CustomTooltip from "./CustomTooltip";
 import CustomBar from "./CustomBar";
 
-const data = [
-  {
-    name: "S",
-    date: "Sun, 20 Aug",
-    cal: 500,
-  },
-  {
-    name: "M",
-    date: "Mon, 21 Aug",
-    cal: 750,
-  },
-  {
-    name: "T",
-    date: "Tue, 22 Aug",
-    cal: 600,
-  },
-  {
-    name: "W",
-    date: "Wed, 23 Aug",
-    cal: 650,
-  },
-  {
-    name: "Th",
-    date: "Thu, 24 Aug",
-    cal: 550,
-  },
-  {
-    name: "F",
-    date: "Fri, 25 Aug",
-    cal: 1000,
-  },
-  {
-    name: "S",
-    date: "Sat, 26 Aug",
-    cal: 875,
-  }
-];
-
 const Chart = () => {
+  const [dateRange, setDateRange] = useState('');
+  const [calories, setCalories] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+
+  useEffect(() => {
+    const fetchDateRange = async () => {
+      try {
+        // Fetch date range from the backend API
+        const response = await fetch("your_backend_date_range_url");
+        const result = await response.json();
+        
+        // Assuming the backend returns an object with calories, hours, and minutes
+        setCalories(result.calories);
+        setHours(result.hours);
+        setMinutes(result.minutes);
+        // Set other data as needed...
+
+        // Fetch date range from the backend API
+        const dateRangeResponse = await fetch("your_backend_date_range_url");
+        const dateRangeResult = await dateRangeResponse.json();
+
+        // Assuming the backend returns a single string representing the date range
+        setDateRange(result);
+      } catch (error) {
+        console.error("Error fetching date range:", error);
+      }
+    };
+
+    fetchDateRange();
+  }, []);
+
+  const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+        return (
+            <div className="custom-tooltip bg-white rounded pb-1 w-[132px] h-[65px]">
+                <p className="label font-roboto-mono text-sm px-[16px] pb-0">{`${payload[0].payload.date}`}</p>
+                <p className="intro font-roboto-mono font-bold px-[16px] text-xl">{`${payload[0].payload.cal} cal`}</p>
+            </div>
+        );
+    }
+    return null;
+  };
+  
   return (
     <>
       {/* mt-24 mr-24 ml-56 mb-40 */}
@@ -56,7 +61,7 @@ const Chart = () => {
         <div className="flex flex-col mt-[24px] w-512 h-400 lg:h-430">
           <heading className="w-156 h-41 mt-0 ml-[16px] lg:ml-[24px] absolute">
             <p className="text-white ">Weekly Performance</p>
-            <p className="text-black-light ">(20 Sun - 26 Sat)</p>
+            <p className="text-black-light ">{dateRange}</p>
           </heading>
 
           {/* destop */}
@@ -99,8 +104,6 @@ const Chart = () => {
               <XAxis dataKey="name" stroke = '#b4b5b7'/>
               <YAxis domain={[0, 1000]} stroke = '#b4b5b7'/>
               <Tooltip
-                //labelStyle={{width: 70, height:49} }
-                // wrapperStyle={{ backgroundColor: '#000' }} 
                 cursor={{ fill: 'transparent' }}
                 content={<CustomTooltip />}
 
@@ -130,4 +133,21 @@ const Chart = () => {
   )
 }
 
-export default Chart
+
+// const Header = () => {
+//   const currentDate = new Date();
+//   const currentWeekday = currentDate.toLocaleString("default", {weekday: "long",});
+//   const currentDay = currentDate.getDate();
+//   const currentMonth = currentDate.toLocaleString("default", {month: "short",});
+//   return (
+//     <div>
+//       <h1 className="text-32 font-bold font-orbitron mt-10 sm:mt-14  md:mt-0">Dashboard</h1>
+//       <p className="font-roboto-mono">
+//         {currentWeekday}, {currentDay} {currentMonth}
+//       </p>
+//     </div>
+//   );
+// };
+
+
+export default Chart;
