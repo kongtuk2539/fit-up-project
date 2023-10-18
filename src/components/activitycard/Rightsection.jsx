af0b4fb81ca8335f3ae51fd1c5cfc9e51a2bb9c9
mallikanns <137501517+mallikanns@users.noreply.github.com>
2023-10-18 17:24:02 +0700

    mook-add async/await

--- src/components/activitycard/Rightsection.jsx
+++ src/components/activitycard/Rightsection.jsx
@@ -10,29 +10,50 @@ const Rightsection = () => {
   const [activities, setActivities] = useState([]);
   
  
-    const getTodayDataById = () => {
-      // รอรับค่า id
-      axios
-        .get(
+    // const getTodayDataById = () => {
+    //   // รอรับค่า id
+    //   axios
+    //     .get(
+    //       "https://fit-up-project-backend.onrender.com/activities/getToday/651edfb83fcee8250bbe6df1"
+    //     )
+    //     .then((response) => {
+    //       console.log(response.data);
+    //       setActivities(response.data);
+    //       if (activities.length > 0) {
+    //         setHaveCard(true);
+    //         console.log('length');
+    //       }
+    //       console.log('set', activities);
+    //     })
+    //     .catch((error) => {
+    //       // Handle any errors from the request
+    //       console.error("Error fetching data: ", error);
+    //     })
+    //     .finally(() => {
+    //       // Any cleanup or final tasks can be done here
+    //       // This block will be executed regardless of whether the request was successful or failed
+    //     });
+    // };
+
+    const getTodayDataById = async () => {
+      try {
+        const response = await axios.get(
           "https://fit-up-project-backend.onrender.com/activities/getToday/651edfb83fcee8250bbe6df1"
-        )
-        .then((response) => {
-          console.log(response.data);
-          setActivities(response.data);
-          if (activities.length > 0) {
-            setHaveCard(true);
-            console.log('length');
-          }
-          console.log('set', activities);
-        })
-        .catch((error) => {
-          // Handle any errors from the request
-          console.error("Error fetching data: ", error);
-        })
-        .finally(() => {
-          // Any cleanup or final tasks can be done here
-          // This block will be executed regardless of whether the request was successful or failed
-        });
+        );
+        console.log(response.data);
+        setActivities(response.data);
+        if (response.data.length > 0) {
+          setHaveCard(true);
+          console.log('length');
+        }
+        console.log('set', response.data);
+      } catch (error) {
+        // Handle any errors from the request
+        console.error("Error fetching data: ", error);
+      } finally {
+        // Any cleanup or final tasks can be done here
+        // This block will be executed regardless of whether the request was successful or failed
+      }
     };
 
     useEffect(()=> {getTodayDataById()},[]);
@@ -50,7 +71,6 @@ const Rightsection = () => {
         ) : (
           <Default />
         )}
-        {/* {haveCard && <Createmobile />} */}
       </div> 
     </>
   );
