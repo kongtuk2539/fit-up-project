940ad9e54f4201e2abecc0e0abf4e8c173efd5b2
mallikanns <137501517+mallikanns@users.noreply.github.com>
2023-10-19 11:05:07 +0700

    mook-add activity form

--- src/components/activitycard/Rightsection.jsx
+++ src/components/activitycard/Rightsection.jsx
@@ -33,24 +33,6 @@ const Rightsection = () => {
     }
   };
 
-  // const getTodayDataById = async () => {
-  //   try {
-  //     const response = await axios.get(
-  //       "https://fit-up-project-backend.onrender.com/activities/getToday/651edfb83fcee8250bbe6df1"
-  //     );
-  //     console.log(response.data);
-  //     setActivities(response.data);
-  //     if (response.data.length > 0) {
-  //       setHaveCard(true);
-  //     }
-  //   } catch (error) {
-  //     console.error("Error fetching data: ", error);
-  //   } finally {
-  //     // Any cleanup or final tasks can be done here
-  //     // This block will be executed regardless of whether the request was successful or failed
-  //   }
-  // };
-
   useEffect(() => {
     getTodayDataById();
   }, [reload]);
@@ -61,8 +43,8 @@ const Rightsection = () => {
         {/* <Card/> */}
         {haveCard ? (
           <>
+            <Createmobile activities={activities} />
             <Card activities={activities} setActivities={setActivities} reload={reload} setReload={setReload} />
-            <Createmobile />
           </>
         ) : (
           <Default />
