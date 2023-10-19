e229ca24e93922bf2d063ae3a240259ce78b7bf9
kongtuk2539 <136842573+kongtuk2539@users.noreply.github.com>
2023-10-19 11:16:13 +0700

    Dev/tho (#34)
    * Update AuthContext.jsx
    
    * axiosService

--- src/components/activitycard/Rightsection.jsx
+++ src/components/activitycard/Rightsection.jsx
@@ -2,36 +2,71 @@ import React, { useState, useEffect } from "react";
 import Card from "./Card";
 import Createmobile from "./Createmobile";
 import Default from "./Default";
-import axios from "axios";
+import axiosService from "../../service/axiosService";
 
 const Rightsection = () => {
   const [haveCard, setHaveCard] = useState(false);
   const [activities, setActivities] = useState([]);
   const [reload, setReload] = useState(false);
 
-
   const getTodayDataById = async () => {
     try {
-      const response = await axios.get(
-        "https://fit-up-project-backend.onrender.com/activities/getToday/651edfb83fcee8250bbe6df1",
-        {
-          headers: {
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTJlYjE0NjYwYzQ0MGY4MzFkYTJhNTAiLCJmaXJzdG5hbWUiOiJpc0xvYWRpbmdfMTEiLCJsYXN0bmFtZSI6ImlzTG9hZGluZ18xMSIsInVzZXJuYW1lIjoiaXNMb2FkaW5nXzExIiwiZW1haWwiOiJpc0xvYWRpbmdfMTFAMTIzNC5jb20iLCJpYXQiOjE2OTc2ODUwMDUsImV4cCI6MTY5NzY4ODYwNX0.1WCrUqU_9JDZqGocW_fnXOiRYci_Ov6dATHHyOT-Y2k'
-          }
-        }
-      );
-      console.log(response.data);
-      setActivities(response.data);
-      if (response.data.length > 0) {
+      const method = 'GET';
+      const url = `https://fit-up-project-backend.onrender.com/activities/getToday/651edfb83fcee8250bbe6df1`;
+      const body = {}
+
+      const response = await axiosService(method, url, body);
+      setActivities(response);
+
+      if (response.length > 0) {
         setHaveCard(true);
       }
+
     } catch (error) {
-      console.error("Error fetching data: ", error);
-    } finally {
-      // Any cleanup or final tasks can be done here
-      // This block will be executed regardless of whether the request was successful or failed
+      console.error('Error fetching data:', error);
     }
   };
+
+  // const getTodayDataById = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       "https://fit-up-project-backend.onrender.com/activities/getToday/651edfb83fcee8250bbe6df1",
+  //       {
+  //         headers: {
+  //           Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTJlYjE0NjYwYzQ0MGY4MzFkYTJhNTAiLCJmaXJzdG5hbWUiOiJpc0xvYWRpbmdfMTEiLCJsYXN0bmFtZSI6ImlzTG9hZGluZ18xMSIsInVzZXJuYW1lIjoiaXNMb2FkaW5nXzExIiwiZW1haWwiOiJpc0xvYWRpbmdfMTFAMTIzNC5jb20iLCJpYXQiOjE2OTc2ODUwMDUsImV4cCI6MTY5NzY4ODYwNX0.1WCrUqU_9JDZqGocW_fnXOiRYci_Ov6dATHHyOT-Y2k'
+  //         }
+  //       }
+  //     );
+  //     console.log(response.data);
+  //     setActivities(response.data);
+  //     if (response.data.length > 0) {
+  //       setHaveCard(true);
+  //     }
+  //   } catch (error) {
+  //     console.error("Error fetching data: ", error);
+  //   } finally {
+  //     // Any cleanup or final tasks can be done here
+  //     // This block will be executed regardless of whether the request was successful or failed
+  //   }
+  // };
+
+  // const getTodayDataById = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       "https://fit-up-project-backend.onrender.com/activities/getToday/651edfb83fcee8250bbe6df1"
+  //     );
+  //     console.log(response.data);
+  //     setActivities(response.data);
+  //     if (response.data.length > 0) {
+  //       setHaveCard(true);
+  //     }
+  //   } catch (error) {
+  //     console.error("Error fetching data: ", error);
+  //   } finally {
+  //     // Any cleanup or final tasks can be done here
+  //     // This block will be executed regardless of whether the request was successful or failed
+  //   }
+  // };
 
   useEffect(() => {
     getTodayDataById();
