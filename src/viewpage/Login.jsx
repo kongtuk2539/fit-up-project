cdaa96fd88ef7686f597283feac0ffa11a657f67
mallikanns <137501517+mallikanns@users.noreply.github.com>
2023-10-21 01:03:06 +0700

    login-mailtype

--- src/viewpage/Login.jsx
+++ src/viewpage/Login.jsx
@@ -113,7 +113,7 @@ const Login = () => {
                 Email
               </label>
               <input
-                type="email"
+                type="text"
                 id="email"
                 name="email"
                 value={formData.email}
